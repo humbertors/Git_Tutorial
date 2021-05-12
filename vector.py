c5afa87397d9fd4ee6592b4e9b6d6573ee610379
Humberto Rodrigues Santos <humbertors@petrobras.com.br>
2021-05-12 08:20:52 -0300

    Adicionada classe Vector

--- vector.py
+++ vector.py
@@ -1,3 +1,14 @@
 [08:17] Andre Mariano
-"""Módulo vector.py Addition:  >>> v1 = Vector(2, 4)  >>> v2 = Vector(2, 1)  >>> v1 + v2  Vector(4, 5) Absolute value::  >>> v = Vector(3, 4)  >>> abs(v)  5.0 Scalar multiplication::  >>> v * 3  Vector(9, 12)  >>> abs(v * 3)  15.0 """
+"""Módulo vector.py Addition:
+>>> v1 = Vector(2, 4)
+>>> v2 = Vector(2, 1)
+>>> v1 + v2
+Vector(4, 5)
+Absolute value::
+>>> v = Vector(3, 4)
+>>> abs(v)  5.0 Scalar multiplication::
+>>> v * 3  Vector(9, 12)
+>>> abs(v * 3)  15.0 """
 
+class Vector:
+    pass
